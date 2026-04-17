14f6282210bad0e3f28c9365601411ad06acd86e
LynnKari F. <149932446+Firrows035@users.noreply.github.com>
2026-04-17 21:23:55 +0800

    创建 choiceSlot.js（日后有用）

---
+++ choiceSlot.js
@@ -0,0 +1,17 @@
+var choiceSlot=[];
+function addChoiceSlot(slotName,usage,x,y,width,height,description,font,color,onClick){
+    choiceSlot.push({
+        slotName:slotName,
+        usage:usage,
+        X:x,
+        Y:y,
+        width:width,
+        height:height,
+        description:{
+            content:description,
+            font:font,
+            color:color,
+        },
+        onClick:onClick,
+    }
+}
