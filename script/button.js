0af94ac4e6e01f946c21409bedeb66b20ba961a5
LynnKari F. <149932446+Firrows035@users.noreply.github.com>
2026-04-28 13:18:59 +0800

    Refactor button definitions and tweak behavior
    Wrap each button.push(...) into named var <Name>B = new function() { ... } blocks and push the same button objects, consolidating duplicated entries (removed a duplicate restart). Adjusted UI selector positions/sizes for several buttons and moved the start resource-loading console.log into the proper branch. Added !skillReady guards for pause/options isDisplayed checks and commented-out the options onClick action. Minor reformatting and ordering changes to improve structure without changing core button logic.

--- script/button.js
+++ script/button.js
@@ -1,205 +1,217 @@
 var button=[];
-button.push({
-    id:"start",
-    attendance:["startReady"],
-    isDisplayed(){
-        if(this.attendance.find((atd)=>atd==currentStage)!=undefined){
-            return 1;
-        }else{
-            return 0;
-        }
-    },
-    selector:{
-        type:"button",
-        offsetX:925,
-        offsetY:700,
-        width:200,
-        height:60,
-        color:"blue",
-        description:{
-            id:"start",
-            icon:"None",
-            text:"Start"
-        },
-    },
-    onMouseOver(){
-        drawRect(2,this.selector.color,this.selector.offsetX+2,this.selector.offsetY+2,this.selector.width-4,this.selector.height-4);
-    },
-    onClick(){
-        if(currentStage=="startReady"){
-            if(imageReady){
-                currentStage="prologue";
-                audio.play();
-                characterPage();  
-                return 1;      
+var startB=new function(){
+    button.push({
+        id:"start",
+        attendance:["startReady"],
+        isDisplayed(){
+            if(this.attendance.find((atd)=>atd==currentStage)!=undefined){
+                return 1;
             }else{
-                console.log(`Loading resources. Please wait...`);
                 return 0;
             }
-        }
-    }
-})
-button.push({
-    id:"restart",
-    attendance:["failure"],
-    isDisplayed(){
-        if(this.attendance.find((atd)=>atd==currentStage)!=undefined){
-            return 1;
-        }else{
-            return 0;
-        }
-    },
-    selector:{
-        type:"button",
-        offsetX:875,
-        offsetY:700,
-        width:300,
-        height:60,
-        color:"blue",
-        description:{
-            id:"restart",
-            icon:"None",
-            text:"Re-Start"
         },
-    },
-    onMouseOver(){
-        drawRect(2,this.selector.color,this.selector.offsetX+2,this.selector.offsetY+2,this.selector.width-4,this.selector.height-4);
-    },
-    onClick(){
-        if(currentStage=="failure"){
-            initialize();
-        }
-    }
-})
-button.push({
-    id:"continue",
-    attendance:["pause"],
-    isDisplayed(){
-        if(this.attendance.find((atd)=>atd==currentStage)!=undefined){
-            return 1;
-        }else{
-            return 0;
-        }
-    },
-    selector:{
-        type:"button",
-        offsetX:875,
-        offsetY:700,
-        width:300,
-        height:60,
-        color:"blue",
-        description:{
-            id:"continue",
-            icon:"None",
-            text:"continue"
+        selector:{
+            type:"button",
+            offsetX:925,
+            offsetY:700,
+            width:200,
+            height:60,
+            color:"blue",
+            description:{
+                id:"start",
+                icon:"None",
+                text:"Start"
+            },
         },
-    },
-    onMouseOver(){
-        drawRect(2,this.selector.color,this.selector.offsetX+2,this.selector.offsetY+2,this.selector.width-4,this.selector.height-4);
-    },
-    onClick(){
-        if(currentStage=="pause"){
-            currentStage=lastStage;
-            audio.play();
+        onMouseOver(){
+            drawRect(2,this.selector.color,this.selector.offsetX+2,this.selector.offsetY+2,this.selector.width-4,this.selector.height-4);
+        },
+        onClick(){
+            if(currentStage=="startReady"){
+                if(imageReady){
+                    currentStage="prologue";
+                    audio.play();
+                    characterPage();  
+                    return 1;      
+                }else{
+                    console.log(`Loading resources. Please wait...`);
+                    return 0;
+                }
+            }
+        }
+    })    
+}
+var restartB=new function(){
+    button.push({
+        id:"restart",
+        attendance:["failure"],
+        isDisplayed(){
+            if(this.attendance.find((atd)=>atd==currentStage)!=undefined){
+                return 1;
+            }else{
+                return 0;
+            }
+        },
+        selector:{
+            type:"button",
+            offsetX:875,
+            offsetY:700,
+            width:300,
+            height:60,
+            color:"blue",
+            description:{
+                id:"restart",
+                icon:"None",
+                text:"Re-Start"
+            },
+        },
+        onMouseOver(){
+            drawRect(2,this.selector.color,this.selector.offsetX+2,this.selector.offsetY+2,this.selector.width-4,this.selector.height-4);
+        },
+        onClick(){
+            if(currentStage=="failure"){
+                initialize();
+            }
+        }
+    })    
+}
+var continueB=new function(){
+    button.push({
+        id:"continue",
+        attendance:["pause"],
+        isDisplayed(){
+            if(this.attendance.find((atd)=>atd==currentStage)!=undefined){
+                return 1;
+            }else{
+                return 0;
+            }
+        },
+        selector:{
+            type:"button",
+            offsetX:875,
+            offsetY:700,
+            width:300,
+            height:60,
+            color:"blue",
+            description:{
+                id:"continue",
+                icon:"None",
+                text:"continue"
+            },
+        },
+        onMouseOver(){
+            drawRect(2,this.selector.color,this.selector.offsetX+2,this.selector.offsetY+2,this.selector.width-4,this.selector.height-4);
+        },
+        onClick(){
+            if(currentStage=="pause"){
+                currentStage=lastStage;
+                audio.play();
+                onMouseMove();
+            }
+        }
+    })    
+}
+var pauseB=new function(){
+    button.push({
+        id:"pause",
+        attendance:["battle","intermission"],
+        isDisplayed(){
+            if(this.attendance.find((atd)=>atd==currentStage)!=undefined&&!skillReady){
+                return 1;
+            }else{
+                return 0;
+            }
+        },
+        selector:{
+            type:"button",
+            offsetX:1050,
+            offsetY:950,
+            width:100,
+            height:40,
+            color:"red",
+            description:{
+                id:"pause",
+                icon:"None",
+                text:"pause"
+            },
+        },
+        onMouseOver(){
+            drawRect(2,this.selector.color,this.selector.offsetX+2,this.selector.offsetY+2,this.selector.width-4,this.selector.height-4);
+        },
+        onClick(){
+            lastStage=currentStage;
+            currentStage="pause";
+            audio.pause();
             onMouseMove();
         }
-    }
-})
-button.push({
-    id:"pause",
-    attendance:["battle","intermission"],
-    isDisplayed(){
-        if(this.attendance.find((atd)=>atd==currentStage)!=undefined){
-            return 1;
-        }else{
-            return 0;
-        }
-    },
-    selector:{
-        type:"button",
-        offsetX:850,
-        offsetY:900,
-        width:250,
-        height:60,
-        color:"red",
-        description:{
-            id:"pause",
-            icon:"None",
-            text:"pause"
+    })    
+}
+var optionsB=new function(){
+    button.push({
+        id:"options",
+        attendance:["battle","intermission","prologue"],
+        isDisplayed(){
+            if(this.attendance.find((atd)=>atd==currentStage)!=undefined&&!skillReady){
+                return 1;
+            }else{
+                return 0;
+            }
         },
-    },
-    onMouseOver(){
-        drawRect(2,this.selector.color,this.selector.offsetX+2,this.selector.offsetY+2,this.selector.width-4,this.selector.height-4);
-    },
-    onClick(){
-        lastStage=currentStage;
-        currentStage="pause";
-        audio.pause();
-        onMouseMove();
-    }
-})
-button.push({
-    id:"restart",
-    attendance:["failure"],
-    isDisplayed(){
-        if(this.attendance.find((atd)=>atd==currentStage)!=undefined){
-            return 1;
-        }else{
-            return 0;
-        }
-    },
-    selector:{
-        type:"button",
-        offsetX:875,
-        offsetY:700,
-        width:300,
-        height:60,
-        color:"blue",
-        description:{
-            id:"restart",
-            icon:"None",
-            text:"Re-Start"
+        selector:{
+            type:"button",
+            offsetX:940,
+            offsetY:900,
+            width:150,
+            height:40,
+            color:"blue",
+            description:{
+                id:"options",
+                icon:"None",
+                text:"options"
+            },
         },
-    },
-    onMouseOver(){
-        drawRect(2,this.selector.color,this.selector.offsetX+2,this.selector.offsetY+2,this.selector.width-4,this.selector.height-4);
-    },
-    onClick(){
-        if(currentStage=="failure"){
-            initialize();
-        }
-    }
-})
-button.push({
-    id:"nextStage",
-    attendance:["intermission"],
-    isDisplayed(){
-        if(this.attendance.find((atd)=>atd==currentStage)!=undefined&&choiceChosen){
-            return 1;
-        }else{
-            return 0;
-        }
-    },
-    selector:{
-        type:"button",
-        offsetX:550,
-        offsetY:500,
-        width:400,
-        height:80,
-        color:"blue",
-        description:{
-            id:"nextStage",
-            icon:"None",
-            text:"next stage"
+        onMouseOver(){
+            drawRect(2,this.selector.color,this.selector.offsetX+2,this.selector.offsetY+2,this.selector.width-4,this.selector.height-4);
         },
-    },
-    onMouseOver(){
-        drawRect(2,this.selector.color,this.selector.offsetX+2,this.selector.offsetY+2,this.selector.width-4,this.selector.height-4);
-    },
-    onClick(){
-        if(currentStage=="intermission"&&choiceChosen){
-            currentStage="battle";
-            beginRound();
+        onClick(){
+            // lastStage=currentStage;
+            // currentStage="setting";
+            // onMouseMove();
         }
-    }
-})
+    })
+}
+var nextStageB=new function(){
+    button.push({
+        id:"nextStage",
+        attendance:["intermission"],
+        isDisplayed(){
+            if(this.attendance.find((atd)=>atd==currentStage)!=undefined&&choiceChosen){
+                return 1;
+            }else{
+                return 0;
+            }
+        },
+        selector:{
+            type:"button",
+            offsetX:550,
+            offsetY:500,
+            width:400,
+            height:80,
+            color:"blue",
+            description:{
+                id:"nextStage",
+                icon:"None",
+                text:"next stage"
+            },
+        },
+        onMouseOver(){
+            drawRect(2,this.selector.color,this.selector.offsetX+2,this.selector.offsetY+2,this.selector.width-4,this.selector.height-4);
+        },
+        onClick(){
+            if(currentStage=="intermission"&&choiceChosen){
+                currentStage="battle";
+                beginRound();
+            }
+        }
+    })    
+}
